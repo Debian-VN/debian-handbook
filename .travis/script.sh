5fa02ddb42a4a3de38ced522119daad940b80498
Giap Tran <txgvnn@gmail.com>
2018-01-25 16:09:42 +0700

    Clarify the travis logs

--- .travis/script.sh
+++ .travis/script.sh
@@ -2,7 +2,8 @@
 ret=0
 for _FILE in vi-VN/*.po;
 do
-    msgfmt --check --output-file /dev/null --statistics ${_FILE};
+    echo "+${_FILE}"
+    msgfmt --check --output-file /dev/null --statistics ${_FILE}
     ret=$(( ret + $? ))
 done
 exit $ret
